ab47e72524b6117289a946783810f3f280375812
Thor Summerall <thor.summerall@gmail.com>
2020-12-01 03:36:10 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    BRANDS << brand if !BRANDS.include? (brand)
   end
 
   def cobble
@@ -13,8 +14,4 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brands=(brands)
-    @brand = brands
-    BRANDS << brands
-  end
 end
